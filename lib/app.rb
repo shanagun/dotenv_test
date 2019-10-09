5ff71d9c314e28ce3ca3e4242559bd1c75fee501
Pak <andrei-pak@outlook.fr>
2019-10-09 17:46:54 +0200

    modif app et Gemfile

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,56 @@
-require 'dotenv'# Appelle la gem Dotenv
+ # ligne très importante qui appelle la gem.
+ require 'twitter'
+ require 'dotenv'
+Dotenv.load
 
-Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
-# et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
+ # n'oublie pas les lignes pour Dotenv ici…
+ 
+ # quelques lignes qui appellent les clés d'API de ton fichier .env
+def stream_client
+ client = Twitter::Streaming::Client.new do |config|
+   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ end
+end
 
-# Il est ensuite très facile d'appeler les données du hash ENV, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
-puts ENV['TWITTER_API_SECRET']
+def client
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+ end
+ 
+ # ligne qui permet de tweeter sur ton compte
+ def say_hello(client)
+  list_artists = ["@donaldglover", "@kidcudi", "@playboicarti", "@trvisxx", "@gucci1017", "@postmalone", "@nickiminaj", "@tylerthecreator", "@iamcardib", "@kendricklamar", "@drake", "@jcolenc", "@kanyewest", "@asvpxrocky", "@theweeknd", "@brysontiller", "@meekmill", "@treysongz", "@migos", "@offsetyrn", "@quavostuntin", "@chrisbrown", "@virgilabloh", "@wizkidayo", "@majidjordan"]
+  5.times do
+    random_artists = list_artists[rand(list_artists.length)]
+    client.update("#{random_artists} su-per ! #bonjour_monde")
+ end
+end
 
-#Autre exemple 
-puts ENV['BEST_WEBSITE_EVER']
+def favorite(client)
+  client.search("#bonjour_monde", result_type:"recent").take(25).collect do |tweet| 
+  client.fav tweet
+ end
+end
+
+def follow(client)
+  client.search("#bonjour_monde", result_type:"recent").take(25).collect do |tweet|
+  return tweet.user.screen_name
+  client.follow tweet.user.screen_name
+ end
+end
+
+def stream(stream_client, client)
+    stream_client.filter(track: "#bonjour_monde") do |object|
+    puts object.text if object.is_a?(Twitter::Tweet)
+    client.fav object
+    client.follow object.user.screen_name
+  end
+end
+stream(stream_client, client)
